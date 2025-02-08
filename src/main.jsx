af430e91dea6b51fcfc57f3e5f8f0d5dc828235b
jp1648 <jaypat0885@gmail.com>
2025-02-07 20:11:34 -0500

    adding vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 
 const theme = createTheme();
 
@@ -10,5 +11,6 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
     <App />
+    <Analytics />
   </ThemeProvider>
 );
